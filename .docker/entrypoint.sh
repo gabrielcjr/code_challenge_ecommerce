a16c37ff833d1dad2fe31054ea48a458896fb382
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2026-07-30 16:36:35 -0300

    refactor: fix filter rendering, drag and drop csv and entrypoint for auto reload

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 python manage.py migrate
 python manage.py seed --refresh || true
+python manage.py collectstatic --noinput || true
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --reload
+exec python manage.py runserver 0.0.0.0:8000
